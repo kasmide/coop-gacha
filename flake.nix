e668781375e5dc431122cb7009c2e7178bb4ffd1
kasmide <email@ksmd.dev>
2025-05-26 19:36:32 +0900

    ci: make dependencies available in PATH

--- flake.nix
+++ flake.nix
@@ -17,8 +17,9 @@
         type = "app";
         program = "${pkgs.writeShellScriptBin "generate-menu" ''
           set -e
-          ${pkgs.curl}/bin/curl -L "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/jobs/artifacts/$CI_COMMIT_BRANCH/download?job=generate-menu" | ${pkgs.libarchive}/bin/bsdtar -x
-          ${pkgs.nushell}/bin/nu ./tools/retrieve_menu.nu
+          export PATH=${pkgs.curl}/bin:${pkgs.libarchive}/bin:${pkgs.nushell}/bin:$PATH
+          curl -L "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/jobs/artifacts/$CI_COMMIT_BRANCH/download?job=generate-menu" | bsdtar -x
+          nu ./tools/retrieve_menu.nu
         ''}/bin/generate-menu";
       };
     };
